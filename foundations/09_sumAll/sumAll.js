6c43365a7e7cad392dd9108fc82f57a4e781e522
Bowen <bowenxu.09.10@gmail.com>
2025-12-30 20:57:39 +0800

    Finish

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== 'number' || typeof end !== 'number'
+        || start < 0 || end < 0
+        || start % 1 != 0 || end % 1 != 0
+    ) {
+        return "ERROR";
+    }
 
+    let bigger, smaller;
+    let sum = 0;
+    start > end ? smaller = end: smaller = start;
+    start > end ? bigger = start : bigger = end;
+
+    for (let i = smaller; i <= bigger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
