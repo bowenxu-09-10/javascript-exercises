faa2f1f5ed0729e39d39bc31521c389d24d3f15a
Bowen <bowenxu.09.10@gmail.com>
2025-12-30 20:38:06 +0800

    Finish

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, target, ...moreTarget) {
+    let result = arr.filter(val => val !== target);
+    for (let item of moreTarget) {
+        result = result.filter(val => val !== item);
+    }
+    return result;
 };
 
 // Do not edit below this line
